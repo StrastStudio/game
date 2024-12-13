335a2e562a5e1380d14c67ccf4924d7ee9b944e7
Селедка под шубой <wewewe2102@gmail.com>
2024-12-13 19:51:28 +0300

    sadsadsadad

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
+import menu
+from menu import*
 from tkinter import *
-from pickle import load, dump
-
-
 
 def set_status(text, color='black'):
     canvas.itemconfig(text_id, text=text, fill=color)
 
-
-def pause_toggle():
-    global pause
-    pause = not pause
-    if pause:
-        set_status('ПАУЗА')
-    else:
-        set_status('ВПЕРЕД!')
-
 def key_handler(event):
+    if event.keycode == KEY_UP or event.keycode == KEY_UP_2:
+        menu.menu_up(canvas)
+    if event.keycode == KEY_DOWN or event.keycode == KEY_DOWN_2:
+        menu.menu_down(canvas)
+    if event.keycode == KEY_ENTER:
+        menu.menu_enter(canvas, player1, player2, text_id)
+
     if game_over:
         return
-    if event.keycode == KEY_PAUSE:
-        pause_toggle()
 
-    if pause:
+    if event.keycode == KEY_PAUSE or event.keycode == KEY_PAUSE_2:
+        menu.pause_toggle()
+        set_status('Пауза!')
+
+    if menu.pause:
+        return
+
+    if event.keycode == KEY_ESC:
+        menu.menu_toggle(canvas)
+
+    if menu_mode:
         return
 
     set_status('Вперед!')
+
     if event.keycode == KEY_PLAYER1:
         canvas.move(player1, SPEED, 0)
     if event.keycode == KEY_PLAYER2:
         canvas.move(player2, SPEED, 0)
 
     check_finish()
-
 
 def check_finish():
     global game_over
@@ -51,100 +56,41 @@ def check_finish():
         set_status('Победа нижнего игрока', player2_color)
         game_over = True
 
+game_width = menu.game_width
+game_height = menu.game_height
+menu_mode = menu.menu_mode
+menu_options = menu.menu_options
+menu_current_index = menu.menu_current_index
+menu_options_id = menu.menu_options_id
 
-def game_new():
-    # menu_toggle()
-    x1, y1 = 50, 50
-    x2, y2 = x1, y1 + player_size + 100
-    canvas.coords(player1, x1, y1, x1 + player_size,
-                  y1 + player_size)
-    canvas.coords(player2, x2, y2, x2 + player_size,
-                  y2 + player_size)
-    print('Начинаем новую игру')
+KEY_UP = menu.KEY_UP
+KEY_UP_2 = menu.KEY_UP_2
+KEY_DOWN = menu.KEY_DOWN
+KEY_DOWN_2 = menu.KEY_DOWN_2
+KEY_ESC = menu.KEY_ESC
+KEY_ENTER = menu.KEY_ENTER
 
-
-
-
-
-
-
-def game_resume():
-    print('Возобновляем старую игру')
-
-
-
-
-def game_save():
-    print('Сохраняем игру')
-    # 1
-    x1 =  canvas.coords(player1)[0]
-    x2 =  canvas.coords(player2)[0]
-    data = [x1, x2]
-    with open('save.dat', 'wb') as f:
-        dump(data, f)
-        set_status('Сохранено', color='yellow')
-
-
-def game_load():
-    print('Загружаем игру')
-    # 2
-    global x1, x2
-    with open('save.dat', 'rb') as f:
-        data = load(f)
-        x1, x2 = data
-        canvas.coords(player1, x1, y1, x1 + player_size,
-                      y1 + player_size)
-        canvas.coords(player2, x2, y2, x2 + player_size,
-                      y2 + player_size)
-        set_status('Загружено', color='yellow')
-
-
-def game_exit():
-    print('Выходим из игры')
-    exit()
-
-
-
-# область глобальных переменных
-game_width = 800
-game_height = 800
-
-
-KEY_UP = 87
-KEY_DOWN = 83
-KEY_ESC = 27
-KEY_ENTER = 13
-
-player_size = 100
-x1, y1 = 50, 50
+player_size = menu.player_size
+x1, y1 = menu.x1, y1
 x2, y2 = x1, y1 + player_size + 100
-player1_color = 'red'
-player2_color = 'blue'
+player1_color = menu.player1_color
+player2_color = menu.player2_color
 
 x_finish = game_width - 50
 
-KEY_PLAYER1 = 39
-KEY_PLAYER2 = 68
-KEY_PAUSE = 32
+KEY_PLAYER1 = menu.KEY_PLAYER1
+KEY_PLAYER2 = menu.KEY_PLAYER2
+KEY_PAUSE = menu.KEY_PAUSE
+KEY_PAUSE_2 = menu.KEY_PAUSE_2
+SPEED = menu.SPEED
 
-SPEED = 12
-
-game_over = False
-pause = False
-
-
-
-
-
-
-
-game_width = 800
-game_height = 800
+game_over = menu.game_over
+pause = menu.pause
 window = Tk()
-window.title('Меню игры')
-
+window.title('DMEC')
 canvas = Canvas(window, width=game_width, height=game_height, bg='white')
 canvas.pack()
+
 player1 = canvas.create_rectangle(x1,
                                   y1,
                                   x1 + player_size,
@@ -160,13 +106,12 @@ finish_id = canvas.create_rectangle(x_finish,
                                     x_finish + 10,
                                     game_height,
                                     fill='black')
-
 text_id = canvas.create_text(x1,
                              game_height - 50,
                              anchor=SW,
                              font=('Arial', '25'),
                              text='Вперед!')
 
-
 window.bind('<KeyRelease>', key_handler)
+
 window.mainloop()
---
+++ menu.py
@@ -0,0 +1,163 @@
+from pickle import load, dump
+from tkinter import *
+
+def pause_toggle():
+    global pause
+    pause = not pause
+
+def menu_toggle(canvas):
+    global menu_mode
+    menu_mode = not menu_mode
+    if menu_mode:
+        menu_show(canvas)
+    else:
+        menu_hide(canvas)
+
+def menu_enter(canvas, player1, player2, text_id):
+    if menu_current_index == 0:
+        game_resume()
+    elif menu_current_index == 1:
+        game_new(canvas, player1, player2)
+    elif menu_current_index == 2:
+        game_save(canvas, player1, player2, text_id)
+    elif menu_current_index == 3:
+        game_load(canvas, player1, player2, text_id)
+    elif menu_current_index == 4:
+        game_exit()
+    menu_hide(canvas)
+
+
+def game_new(canvas, player1, player2):
+    menu_toggle(canvas)
+    x1, y1 = 50, 50
+    x2, y2 = x1, y1 + player_size + 100
+    canvas.coords(player1, x1, y1, x1 + player_size,
+                  y1 + player_size)
+    canvas.coords(player2, x2, y2, x2 + player_size,
+                  y2 + player_size)
+    print('Начинаем новую игру')
+
+
+
+
+def game_resume():
+    print('Возобновляем старую игру')
+
+
+
+
+def game_save(canvas, player1, player2):
+    print('Сохраняем игру')
+    # 1
+    x1 =  canvas.coords(player1)[0]
+    x2 =  canvas.coords(player2)[0]
+    data = [x1, x2]
+    with open('save.dat', 'wb') as f:
+        dump(data, f)
+        #set_status(canvas, text_id, text = 'Сохранено', color='yellow')
+
+
+def game_load(canvas, player1, player2):
+    print('Загружаем игру')
+    # 2
+    global x1, x2
+    with open('save.dat', 'rb') as f:
+        data = load(f)
+        x1, x2 = data
+        canvas.coords(player1, x1, y1, x1 + player_size,
+                      y1 + player_size)
+        canvas.coords(player2, x2, y2, x2 + player_size,
+                      y2 + player_size)
+
+
+def game_exit():
+    print('Выходим из игры')
+    exit()
+
+
+def menu_show(canvas):
+    global menu_mode, pause
+    menu_mode = True
+    pause = True
+    menu_update(canvas)
+
+def menu_hide(canvas):
+    global menu_mode, pause
+    menu_mode = False
+    pause = False
+    menu_update(canvas)
+
+
+def menu_up(canvas):
+    global menu_current_index
+    menu_current_index -= 1
+    if menu_current_index < 0:
+        menu_current_index = 0
+    menu_update(canvas)
+
+
+def menu_down(canvas):
+    global menu_current_index
+    menu_current_index += 1
+    if menu_current_index > len(menu_options) - 1:
+        menu_current_index = len(menu_options) - 1
+    menu_update(canvas)
+
+
+def menu_update(canvas):
+    for menu_index in range(len(menu_options_id)):
+        element_id = menu_options_id[menu_index]
+        if menu_mode:
+            canvas.itemconfig(element_id, state='normal')
+            if menu_index == menu_current_index:
+                canvas.itemconfig(element_id, fill='blue')
+            else:
+                canvas.itemconfig(element_id, fill='black')
+        else:
+            canvas.itemconfig(element_id, state='hidden')
+
+
+def menu_create(canvas):
+    offest = 0
+    for menu_option in menu_options:
+        option_id = canvas.create_text(400, 200 + offest, anchor=CENTER, font=('Arial', '25'), text=menu_option,
+                                       fill='black')
+        menu_options_id.append(option_id)
+        offest += 50
+    menu_update(canvas)
+
+
+
+
+
+game_width = 800
+game_height = 800
+menu_mode = False # Не переключать! Игра работать не будет!
+menu_options = ['Возврат в игру', 'Новая игра', 'Сохранить', 'Загрузить', 'Выход']
+menu_current_index = 3
+menu_options_id = []
+
+
+KEY_UP = 87
+KEY_UP_2 = 38
+KEY_DOWN = 83
+KEY_DOWN_2 = 40
+KEY_ESC = 27
+KEY_ENTER = 13
+
+player_size = 100
+x1, y1 = 50, 50
+x2, y2 = x1, y1 + player_size + 100
+player1_color = 'red'
+player2_color = 'blue'
+
+x_finish = game_width - 50
+
+KEY_PLAYER1 = 39
+KEY_PLAYER2 = 68
+KEY_PAUSE = 32
+KEY_PAUSE_2 = 19
+SPEED = 12
+
+game_over = False
+pause = False
